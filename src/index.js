32c3ef3b4f08f69daacf6ed1f3eee46b96b8442b
Bharatwaj <bharatwaj@ncompass.inc>
2024-04-12 19:28:57 +0530

    updated the index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     console.log("This is a test lambda");
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Lambda return something..'),
+        body: JSON.stringify('Lambda return something.. updated the lambda post fortst success'),
       };
     return response;
 }
